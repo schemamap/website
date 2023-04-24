b38d91db60269d8278e592d1a85a8ca61a71b7ef
Krisztian Szabo <thenonameguy24@gmail.com>
2023-04-24 09:16:30 +0200

    Add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  site: 'https://schemamap.io',
+  integrations: [sitemap()]
 });
