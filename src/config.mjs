4bb75e9e4507d7b7ad7faa394c89199a6b1a987e
Krisztian Szabo <thenonameguy24@gmail.com>
2023-07-09 18:04:21 +0200

    Updated favicons based off realfavicongenerator.net

--- src/config.mjs
+++ src/config.mjs
@@ -1,26 +1,27 @@
-import defaultImage from './assets/images/default.png';
+import defaultImage from "./assets/images/default.png";
 
 const CONFIG = {
-  name: 'schemamap.io',
+  name: "schemamap.io",
 
-  origin: 'https://schemamap.io',
-  basePathname: '/',
+  origin: "https://schemamap.io",
+  basePathname: "/",
   trailingSlash: false,
 
-  title: 'schemamap.io — Tenant Onboarding for Postgres',
-  description: 'schemamap.io handles your tenant onboarding end-to-end based on your database schema.',
+  title: "schemamap.io — Tenant Onboarding for Postgres",
+  description:
+    "schemamap.io handles your tenant onboarding end-to-end based on your database schema.",
   defaultImage,
 
-  defaultTheme: 'light', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
+  defaultTheme: "light", // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
 
-  language: 'en',
-  textDirection: 'ltr',
+  language: "en",
+  textDirection: "ltr",
 
-  dateFormatter: new Intl.DateTimeFormat('en', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    timeZone: 'UTC',
+  dateFormatter: new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: "UTC",
   }),
 
   microsoftClarityId: "gtgmt0rftj",
@@ -31,25 +32,25 @@ const CONFIG = {
     postsPerPage: 3,
 
     post: {
-      permalink: 'blog/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
+      permalink: "blog/%slug%", // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
       noindex: false,
       disabled: false,
     },
 
     list: {
-      pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
+      pathname: "blog", // Blog main path, you can change this to "articles" (/articles)
       noindex: false,
       disabled: false,
     },
 
     category: {
-      pathname: 'blog/category', // Category main path /category/some-category
+      pathname: "blog/category", // Category main path /category/some-category
       noindex: true,
       disabled: true,
     },
 
     tag: {
-      pathname: 'blog/tag', // Tag main path /tag/some-tag
+      pathname: "blog/tag", // Tag main path /tag/some-tag
       noindex: true,
       disabled: true,
     },
