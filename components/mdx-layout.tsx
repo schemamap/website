8af535c2073f93b5e4113825cace4e11b9913e98
Krisztian Szabo <thenonameguy24@gmail.com>
2024-02-29 14:23:34 +0100

    Remove outdated Prose component

--- components/mdx-layout.tsx
+++ components/mdx-layout.tsx
@@ -1,5 +1,4 @@
 import { Section } from "./section";
-import { Prose } from "@nikolovlazar/chakra-ui-prose";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return <Section innerWidth="container.lg">{children}</Section>;
