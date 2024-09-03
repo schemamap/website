86aaa9982f2098eb64ea8acbc9f5ad375cd0ff36
Krisztian Szabo <thenonameguy24@gmail.com>
2024-09-03 10:11:51 +0200

    Add Apollo tracking script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,14 @@ export default class MyDocument extends Document {
             href="/static/favicons/favicon-16x16.png"
           />
           <link rel="manifest" href="/static/favicons/manifest.json" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"66d6c41d1c745a068366776d"})},
+document.head.appendChild(o)}initApollo();`,
+            }}
+          ></script>
         </Head>
         <body>
           <ColorModeScript initialColorMode={"dark"} type="localStorage" />
